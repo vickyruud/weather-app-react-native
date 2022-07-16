cad2203829ae03345e2631632795631c290e6386
vickyruud <aprajit.k@gmail.com>
2022-07-16 14:01:15 -0400

    add dotenv and set up virtual device

--- App.js
+++ App.js
@@ -1,10 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+import { useEffect, useState } from "react";
+import { WEATHER_API_KEY } from "@env";
 
 export default function App() {
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  async function load() {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== "granted") {
+        setErrorMessage("Location Permission is required!");
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync();
+      const { latitude, longitude } = location.coords;
+      alert(`Latitude: ${latitude}, Longitude: ${longitude}`);
+    } catch (error) {}
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Weather App</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +36,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

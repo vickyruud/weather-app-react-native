dff242ff24267ef8d29c9a75695d6e94999da06b
vickyruud <aprajit.k@gmail.com>
2022-07-16 14:10:41 -0400

    create load function and pull weather data

--- App.js
+++ App.js
@@ -4,8 +4,11 @@ import * as Location from "expo-location";
 import { useEffect, useState } from "react";
 import { WEATHER_API_KEY } from "@env";
 
+const BASE_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?";
+
 export default function App() {
   const [errorMessage, setErrorMessage] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
 
   async function load() {
     try {
@@ -16,8 +19,20 @@ export default function App() {
         return;
       }
       const location = await Location.getCurrentPositionAsync();
+
       const { latitude, longitude } = location.coords;
-      alert(`Latitude: ${latitude}, Longitude: ${longitude}`);
+
+      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`;
+
+      const response = await fetch(weatherUrl);
+
+      const result = await response.json();
+
+      if (response.ok) {
+        setCurrentWeather(result);
+      } else {
+        setErrorMessage(result.message);
+      }
     } catch (error) {}
   }
 
@@ -25,12 +40,19 @@ export default function App() {
     load();
   }, []);
 
-  return (
-    <View style={styles.container}>
-      <Text>Weather App</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  if (currentWeather) {
+
+    const {} = currentWeather
+
+    return (
+      <View style={styles.container}>
+        <Text>Weather App</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+
+
 }
 
 const styles = StyleSheet.create({
